01ff91b980936bb5717d1cedcf0c84a537c562e9
Brandon Day <brandontday7@gmail.com>
2021-04-05 11:47:21 -0400

    better landing page

--- src/components/CdCase/Platforms.js
+++ src/components/CdCase/Platforms.js
@@ -8,8 +8,10 @@ import youtube from "../../images/logos/youtube.png"
 import instagram from "../../images/logos/instagram.png"
 import Spotify from "./Spotify"
 
-const epAppleMusic = "https://music.apple.com/ca/artist/rose-red-youth/1458640768"
-const epSpotify = "https://open.spotify.com/artist/2GnSTnxs3NcFhxJNOrSb92?si=x0SHXrk1S2KqJ4Xm4d1iBQ"
+const epAppleMusic =
+  "https://music.apple.com/ca/artist/rose-red-youth/1458640768"
+const epSpotify =
+  "https://open.spotify.com/artist/2GnSTnxs3NcFhxJNOrSb92?si=x0SHXrk1S2KqJ4Xm4d1iBQ"
 const epTiktok = "https://www.tiktok.com/@rose_red_youth"
 const epSoundcloud = "https://soundcloud.com/roseredyouth"
 const epYoutube = "https://www.youtube.com/channel/UCwOLyxcAVrRVY6DIb96TJmg"
@@ -24,88 +26,67 @@ const fade = keyframes`
 	}
 `
 
-
 const StyledDiv = styled.div`
-	position: absolute;
-	bottom: 0;
-	width: 100%;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	animation: ${fade} 0.8s;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  animation: ${fade} 0.8s;
 
-	@media (max-width: 500px) {
-		flex-direction: column;
-		align-items: initial;
-	}
+  @media (max-width: 500px) {
+    flex-direction: column;
+    align-items: initial;
+  }
 `
 
 const StyledImg = styled.img`
-	width: 30px;
-	margin: 0 1em 1em 0;
-	cursor: pointer;
+  width: 30px;
+  margin: 0 1em 1em 0;
+  cursor: pointer;
 `
 
 const Links = styled.div`
-	display: flex;
-	margin-left: 1em;
+  display: flex;
+  margin-left: 1em;
 
-	@media (max-width: 500px) {
-		.tier-2 {
-			display: none;
-		}
-	}
+  @media (max-width: 500px) {
+    .tier-2 {
+      display: none;
+    }
+  }
 `
 
 const Platforms = () => (
-	<StyledDiv>
-	<Spotify />
-		<Links>
-			<a
-				href={epSpotify} 
-				target="_blank"
-				rel="noopener noreferrer nofollow"
-				className="tier-2"
-				>
-				<StyledImg src={spotify} alt="Spotify Logo"/>
-			</a>
-			<a 
-				href={epAppleMusic} 
-				target="_blank"
-				rel="noopener noreferrer nofollow"
-				>
-				<StyledImg src={apple} alt="Apple Music Logo"/>
-			</a>
-			<a
-				href={epTiktok}
-				target="_blank"
-				rel="noopener noreferrer nofollow"
-				>
-				<StyledImg src={tiktok} alt="Tiktok Logo" />
-			</a>
-			<a
-				href={epYoutube} 
-				target="_blank"
-				rel="noopener noreferrer nofollow"
-				>
-				<StyledImg src={youtube} alt="YouTube Logo"/>
-			</a>
-			<a
-				href={epSoundcloud}
-				target="_blank"
-				rel="noopener noreferrer nofollow"
-			>
-				<StyledImg src={soundcloud} alt="Soundcloud Logo" />
-			</a>
-			<a
-				href={epInstagram}
-				target="_blank"
-				rel="noopener noreferrer nofollow"
-				>
-				<StyledImg src={instagram} alt="Instagram Logo" />
-			</a>
-		</Links>
-	</StyledDiv>
+  <StyledDiv>
+    <Spotify />
+    <Links>
+      <a
+        href={epSpotify}
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+        className="tier-2"
+      >
+        <StyledImg src={spotify} alt="Spotify Logo" />
+      </a>
+      <a href={epAppleMusic} target="_blank" rel="noopener noreferrer nofollow">
+        <StyledImg src={apple} alt="Apple Music Logo" />
+      </a>
+      <a href={epTiktok} target="_blank" rel="noopener noreferrer nofollow">
+        <StyledImg src={tiktok} alt="Tiktok Logo" />
+      </a>
+      <a href={epYoutube} target="_blank" rel="noopener noreferrer nofollow">
+        <StyledImg src={youtube} alt="YouTube Logo" />
+      </a>
+      <a href={epSoundcloud} target="_blank" rel="noopener noreferrer nofollow">
+        <StyledImg src={soundcloud} alt="Soundcloud Logo" />
+      </a>
+      <a href={epInstagram} target="_blank" rel="noopener noreferrer nofollow">
+        <StyledImg src={instagram} alt="Instagram Logo" />
+      </a>
+    </Links>
+  </StyledDiv>
 )
 
 export default Platforms
--- src/components/CdCase/index.js
+++ src/components/CdCase/index.js
@@ -1,139 +1,200 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "react-router-dom"
-import Platforms from "./Platforms"
-import styled from "styled-components"
-import rryEp from "../../images/album-art/rryEp.jpg"
-import iMissSummer from "../../images/album-art/ims.png"
-import letsJustBeFriends from "../../images/album-art/beef.png"
+import styled, { keyframes } from "styled-components"
+import rings from "../../images/logos/rings.png"
 
-import "../Header/header.css"
-import "./cdCase.css"
+import spotifyLogo from "../../images/logos/spotify.png"
+import appleLogo from "../../images/logos/apple.png"
+import tiktokLogo from "../../images/logos/tiktok.png"
+import soundcloudLogo from "../../images/logos/soundcloud.png"
+import youtubeLogo from "../../images/logos/youtube.png"
+import instagramLogo from "../../images/logos/instagram.png"
+import twitterLogo from "../../images/logos/twitter.png"
 
-// const StyledHeading = styled.p`
-// 	font-size: 175%;
-// 	text-decoration: none;
-// 	font-family: "Roslindale";
-// 	src: url("../../fonts/RoslindaleText-Regular.woff") format("woff");
-// 	color: black;
-// 	top: 0;
-// 	right: 0;
-// 	margin: 0.5em 0.5em;
-// 	position: absolute;
-// 	color: ${({ main }) => main ? '#555' : 'transparent'};
-// 	transform: ${({ main }) => main ? 'translate(3px, -3px)' : 'none'};
-// 	-webkit-text-stroke: 1.2px #555;
-
-// 	&:hover {
-// 		color: ${({ main }) => main ? '#777' : 'transparent'};
-// 		-webkit-text-stroke: 1.2px #777;
-// 		transition: 0.7s;
-// 	}
-// `
-
-const StyledArtworkContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 75vw;
-
-  @media (max-width: 500px) {
-    flex-direction: column;
-    width: 70vw;
-    transform: translateY(-100px);
-  }
-`
+const spotify =
+  "https://open.spotify.com/artist/2GnSTnxs3NcFhxJNOrSb92?si=DctWUYaXRwu2MdjVOgSSgQ"
+const appleMusic = "https://music.apple.com/ca/artist/rose-red-youth/1458640768"
+const tiktok = "https://www.tiktok.com/@rose_red_youth"
+const soundcloud = "https://soundcloud.com/roseredyouth"
+const youtube = "https://www.youtube.com/channel/UCwOLyxcAVrRVY6DIb96TJmg"
+const instagram = "https://www.instagram.com/roseredyouthband/"
+const twitter = "https://twitter.com/rose_red_youth"
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: #ececec;
-  font-family: "Roslindale";
-  src: url("../../fonts/RoslindaleText-Regular.woff") format("woff");
-`
-const SingleContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-const Presave = styled.div``
-const SingleTitle = styled.div`
-  font-size: 25px;
-  margin: 5px 0;
+  background-color: #f2f0d6;
+  font-family: "Arial";
 `
 
-const Artwork = styled.img`
-  border-radius: 2px;
-  width: 60px;
-  cursor: pointer;
-  &:hover {
-    background-color: black;
-    opacity: 0.9;
-    -webkit-box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.05);
-    -moz-box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.05);
-    box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.05);
-    transition: 0.3s;
+const bounce = keyframes`
+	from {
+		transform: translateY(1px);
+	}
+	to {
+		transform: translateY(-1px);
+	}
+`
+
+const Eras = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+
+    & > * {
+      margin: 40px 0;
+    }
   }
 `
 
-const SingleArtwork = styled(Artwork)`
-  width: 35vw;
-  max-width: 400px;
+const Rings = styled.img`
+  max-width: 350px;
+
+  animation: bounce 3s infinite;
 `
 
-const PreviousArtwork = styled(Artwork)`
-  margin: 0 5px;
+const Ep = styled.div`
+  cursor: pointer;
+  font-size: 25px;
+  font-family: "Beyond Infinity";
+  src: url("../../fonts/Beyond Infinity.ttf") format("truetype");
 `
 
-const OtherProjects = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  margin: 10px;
+const IMissSummer = styled.div`
+  cursor: pointer;
+  font-size: 25px;
+  font-family: "Roslindale";
+  src: url("../../fonts/RoslindaleText-Regular.woff") format("woff");
+`
+
+const StyledLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+`
+
+const fadeIn = keyframes`
+	from {
+		opactity: 0;
+	}
+	to {
+		opacity: 1;
+	}
+`
+
+const Instructions = styled.div`
+  margin-bottom: 25px;
+  font-size: 12px;
+  animation: fadeIn 3s;
+`
+
+const Center = styled.div`
   display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
+  width: 100vw;
+  position: absolute;
+  bottom: 0;
+  margin-bottom: 20px;
 `
 
-const CdCase = () => (
-  <Container>
-    <OtherProjects>
-      <Link to="/imisssummer">
-        <PreviousArtwork src={iMissSummer} />
-      </Link>
-      <Link to="/ep">
-        <PreviousArtwork src={rryEp} />
-      </Link>
-    </OtherProjects>
-    <SingleContainer>
-      <Presave>Pre-save</Presave>
-      <SingleTitle>Let's Just Be Friends</SingleTitle>
-      <a
-        href="https://distrokid.com/hyperfollow/roseredyouth/lets-just-be-friends"
-        target="blank"
-      >
-        <SingleArtwork src={letsJustBeFriends} />
-      </a>
-    </SingleContainer>
-  </Container>
+const Platforms = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  max-width: 500px;
+`
 
-  // <div className="site-container black">
-  /* <Link to="/store">
-			<div>
-				<StyledHeading main>Store</StyledHeading>
-				<StyledHeading>Store</StyledHeading>
-			</div>
-		</Link> */
-  //   <StyledArtworkContainer className="artwork-container">
-  //     <Link to="/imisssummer">
-  //       <img alt="imisssummer" className="album-art" src={iMissSummer} />
-  //     </Link>
-  //     <Link to="/ep">
-  //       <img alt="rry" className="album-art" src={rryEp} />
-  //     </Link>
-  //   </StyledArtworkContainer>
-  //   <Platforms />
-  // </div>
-)
+const Platform = styled.a`
+  text-decoration: none;
+`
+
+const Icon = styled.img`
+  height: 20px;
+`
+
+class CdCase extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      instructionsVisible: false,
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ instructionsVisible: true }), 5000)
+  }
+
+  render() {
+    return (
+      <Container>
+        <Eras>
+          <StyledLink to="/ep">
+            <Ep>Rose Red Youth Ep</Ep>
+          </StyledLink>
+          <a
+            href="https://distrokid.com/hyperfollow/roseredyouth/lets-just-be-friends"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Rings src={rings} />
+          </a>
+          <StyledLink to="/imisssummer">
+            <IMissSummer>I Miss Summer</IMissSummer>
+          </StyledLink>
+        </Eras>
+        <Center>
+          {this.state.instructionsVisible && (
+            <Instructions>Click an era to view</Instructions>
+          )}
+          <Platforms>
+            <Platform href={spotify} target="_blank" rel="noopener noreferrer">
+              <Icon src={spotifyLogo} />
+            </Platform>
+            <Platform
+              href={appleMusic}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon src={appleLogo} />
+            </Platform>
+            <Platform href={tiktok} target="_blank" rel="noopener noreferrer">
+              <Icon src={tiktokLogo} />
+            </Platform>
+            <Platform
+              href={instagram}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon src={instagramLogo} />
+            </Platform>
+            <Platform href={twitter} target="_blank" rel="noopener noreferrer">
+              <Icon src={twitterLogo} />
+            </Platform>
+            <Platform href={youtube} target="_blank" rel="noopener noreferrer">
+              <Icon src={youtubeLogo} />
+            </Platform>
+            <Platform
+              href={soundcloud}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon src={soundcloudLogo} />
+            </Platform>
+          </Platforms>
+        </Center>
+      </Container>
+    )
+  }
+}
 
 export default CdCase
